c5d79e919bc4c9d2626faf5c9907a75af69c67c6
Maxim Rotar <maxim20003244@gmail.com>
2025-01-31 23:32:40 -0800

    Add RelationShip  between User and Address

--- src/main/java/com/ecommenrce/project/model/Address.java
+++ src/main/java/com/ecommenrce/project/model/Address.java
@@ -9,15 +9,45 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "address")
+@Table(name = "addresses")
 public class Address {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long addressId;
+    @NotBlank
+    @Size(min = 5,message = "Street name must be at less 5 characters")
+    private String street;
+    @NotBlank
+    @Size(min = 5,message = "Building  name must be at less 5 characters")
+    String buildingName;
+    @NotBlank
+    @Size(min = 4,message = "Street name must be at less 4 characters")
+    String city;
+    @NotBlank
+    @Size(min = 2,message = "Street name must be at less 2 characters")
+    String state;
+    @NotBlank
+    @Size(min = 2,message = "Street name must be at less 2 characters")
+    String country;
+    @NotBlank
+    @Size(min = 5,message = "Zipcode name must be at less 5 characters")
+    private String zipcode;
 
+    @ManyToMany(mappedBy = "addresses")
+    private List<Users> users = new ArrayList<>();
 
+    public Address(String street, String buildingName, String city, String state, String country) {
+        this.street = street;
+        this.buildingName = buildingName;
+        this.city = city;
+        this.state = state;
+        this.country = country;
+    }
 }

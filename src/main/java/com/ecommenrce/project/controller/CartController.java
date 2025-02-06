6f987bfdd18a0f9a8aa7bb3aaacb1853d6146fea
Maxim Rotar <maxim20003244@gmail.com>
2025-02-06 12:42:04 -0800

    Fetching All Carts

--- src/main/java/com/ecommenrce/project/controller/CartController.java
+++ src/main/java/com/ecommenrce/project/controller/CartController.java
@@ -5,10 +5,9 @@ import com.ecommenrce.project.service.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -23,5 +22,10 @@ public class CartController {
      CartDTO cartDTO = cartService.addProductToCart(productId,quantity);
      return new  ResponseEntity<CartDTO>(cartDTO, HttpStatus.CREATED);
 
+   }
+@GetMapping("/carts")
+   public ResponseEntity<List<CartDTO>> getCards (){
+        List<CartDTO> cartDTOS = cartService.getAllCarts();
+        return new ResponseEntity<>(cartDTOS,HttpStatus.FOUND);
    }
 }
--- src/main/java/com/ecommenrce/project/service/CartService.java
+++ src/main/java/com/ecommenrce/project/service/CartService.java
@@ -3,9 +3,13 @@ package com.ecommenrce.project.service;
 import com.ecommenrce.project.dto.CartDTO;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 
 public interface CartService {
 
     CartDTO addProductToCart(Long productId, Integer quantity);
+
+    List<CartDTO> getAllCarts();
 }
 
